1b7a3e86ac0bd88a90d2384df05d7cead85a521f
MichaelDerendyaev <derendyaevm1@mail.ru>
2021-10-15 00:39:59 +0300

    Good

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import TicTacToe
 class MyTest(unittest.TestCase):
     def setUp(self) -> None:
         self.game = TicTacToe.TicTacToe()
-        self.samples_of_strings = ("123\n456\n789\n", "OOO\nOOO\nOOO", "XXX\nXXX\nXXX\n",
+        self.samples_of_strings = ("123\n456\n789\n", "OOO\nOOO\nOOO\n", "XXX\nXXX\nXXX\n",
                                    "1OX\n4XO\nOXO\n", "123\nXXX\nOOO\n", "12X\nXOX\nOO9\n")
         self.samples_of_boards = ([0,0,0,0,0,0,0,0,0],[2,2,2,2,2,2,2,2,2],[1,1,1,1,1,1,1,1,1],
                                   [0,2,1,0,1,2,2,1,2],[0,0,0,1,1,1,2,2,2],[0,0,1,1,2,1,2,2,0])
@@ -30,14 +30,14 @@ class MyTest(unittest.TestCase):
         for i in range(len(self.samples_of_boards_for_win_move)):
             self.game.board = self.samples_of_boards_for_win_move[i][0]
             self.game.turn = self.samples_of_boards_for_win_move[i][1]
-            move_pos = self.samples_of_boards_for_win_move[i][3]
+            move_pos = self.samples_of_boards_for_win_move[i][2]
             ans = self.game.win_move(move_pos)
             self.assertEqual(ans, self.samples_of_answers_for_win_move[i])
 
     def test_making_a_turn(self):
         i = 0
         for string in self.samples_of_input_for_turn:
-            print(string)
+            print("----------->"+string+"<-----------")
             ans = self.game.make_turn()
             self.assertEqual(ans, self.samples_of_making_turns[i])
             i += 1
@@ -45,10 +45,14 @@ class MyTest(unittest.TestCase):
     def test_ask_the_repetition(self):
         i = 0
         for string in self.samples_of_asking_the_rep:
-            print(string)
+            print("----------->"+string+"<-----------")
             ans = self.game.ask_for_repetition()
             self.assertEqual(ans, self.samples_of_repetition[i])
             i += 1
 
     def tearDown(self) -> None:
         pass
+
+
+if __name__ == "__main__":
+    unittest.main()
