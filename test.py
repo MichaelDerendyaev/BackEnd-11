015424d2eedc0d04461c14bb57c9fb5dcb14ffda
MichaelDerendyaev <derendyaevm1@mail.ru>
2021-10-20 19:23:44 +0300

    Final

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
-import TicTacToe
+import tic_tac_toe
 
 
 class MyTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.game = TicTacToe.TicTacToe()
+        self.game = tic_tac_toe.TicTacToe(("Michael_1", "Michael_2"))
         self.samples_of_strings = ("123\n456\n789\n", "OOO\nOOO\nOOO\n", "XXX\nXXX\nXXX\n",
                                    "1OX\n4XO\nOXO\n", "123\nXXX\nOOO\n", "12X\nXOX\nOO9\n")
         self.samples_of_boards = ([0,0,0,0,0,0,0,0,0],[2,2,2,2,2,2,2,2,2],[1,1,1,1,1,1,1,1,1],
@@ -13,12 +13,9 @@ class MyTest(unittest.TestCase):
                                                ([1,2,1,2,0,2,1,2,1],0,5),([1,2,1,2,0,2,1,2,1],1,5),
                                                ([2,2,1,1,1,0,2,2,1],1,6),([0,0,0,0,0,0,0,0,0],0,4))
         self.samples_of_answers_for_win_move = (True, True, True, True, False, False)
-        self.samples_of_input_for_turn = ("Введите 9:","Введите 7:","Введите 1:",
-                                          "Введите -1, затем 15, а после 'qwerty'. После всего 5:")
+        self.samples_of_input_for_turn = (("9",),("7",),("1",),("-1","15","qwerty","5"))
         self.samples_of_making_turns = (9, 7, 1, 5)
-        self.samples_of_asking_the_rep = ("Введите 'YES':", "Введите 'NO':",
-                                          "Введите 'Y', затем 'YE', после 'YES':",
-                                          "Введите 5 раз 'What?':")
+        self.samples_of_asking_the_rep = (("YES",),("NO",),("Y","YE","YES"),("1","2","3","4","5"))
         self.samples_of_repetition = (True, False, True, False)
 
     def test_to_string(self):
@@ -32,22 +29,20 @@ class MyTest(unittest.TestCase):
             self.game.turn = self.samples_of_boards_for_win_move[i][1]
             move_pos = self.samples_of_boards_for_win_move[i][2]
             ans = self.game.win_move(move_pos)
-            self.assertEqual(ans, self.samples_of_answers_for_win_move[i])
+            self.assertEqual(ans, self.samples_of_answers_for_win_move[i], msg="Invalid method TicTacToe.win_move()\n")
 
     def test_making_a_turn(self):
         i = 0
         for string in self.samples_of_input_for_turn:
-            print("----------->"+string+"<-----------")
-            ans = self.game.make_turn()
-            self.assertEqual(ans, self.samples_of_making_turns[i])
+            ans = self.game.make_turn(string)
+            self.assertEqual(ans, self.samples_of_making_turns[i], msg="Invalid method TicTacToe.make_turn(s=None)\n")
             i += 1
 
     def test_ask_the_repetition(self):
         i = 0
         for string in self.samples_of_asking_the_rep:
-            print("----------->"+string+"<-----------")
-            ans = self.game.ask_for_repetition()
-            self.assertEqual(ans, self.samples_of_repetition[i])
+            ans = self.game.ask_for_repetition(string)
+            self.assertEqual(ans, self.samples_of_repetition[i], msg="Invalid static method TicTacToe.ask_for_repetition(s=None)\n")
             i += 1
 
     def tearDown(self) -> None:
---
+++ tic_tac_toe.py
@@ -0,0 +1,134 @@
+"""Tic Tac Toe"""
+
+from functools import reduce
+
+
+class TicTacToe:
+    """class of game Tic Tac Toe"""
+    def __init__(self, names=None):
+        """initialization"""
+        if names is None:
+            self.players = [input("Введите имя первого игрока: "),
+                            input("А теперь второго игрока: ")]
+        else:
+            self.players = list(names)
+        self.turn = 0
+        self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    def __str__(self):
+        """converting of the board to a string"""
+        res = ""
+        for i in range(1, 10):
+            j = self.board[i-1]
+            if j == 0:
+                res += str(i)
+            elif j == 1:
+                res += "X"
+            else:
+                res += "O"
+            if i % 3 == 0:
+                res += "\n"
+        return res
+
+    def __repr__(self):
+        """representation of the board"""
+        return str(self)
+
+    def mainloop(self):
+        """main loop of the game"""
+        ans = True
+        while ans:
+            self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            self.turn = 0
+            while True:
+                print(self)
+                pos = self.make_turn()
+                if self.win_move(pos):
+                    print(self)
+                    print(f"{self.players[self.turn % 2]}, поздравляем!")
+                    print(f"Вы не оставили игроку {self.players[(self.turn + 1) % 2]} ни шанса!")
+                    break
+                if self.turn > 8:
+                    print(self)
+                    print(f"Уважаемые игроки {self.players[0]} и {self.players[1]}, поле заполнено")
+                    print("Это означает, что победила ваша крепкая дружба!")
+                    break
+            ans = self.ask_for_repetition()
+
+    def make_turn(self, string=None):
+        """get the move from console"""
+        pos = ""
+        if string is None:
+            pos = input(f"{self.players[self.turn % 2]}, делайте ваш ход: ")
+            while not pos.isdigit() or int(pos) not in range(1, 10) or self.board[int(pos)-1] != 0:
+                print("Неправильный ход, нужно указать цифру от 1 до 9")
+                print("Нельзя также туда ходить, куда ход сделан был")
+                pos = input(f"{self.players[self.turn % 2]}, делайте правильный ход: ")
+        else:
+            for pos in string:
+                if pos.isdigit() and int(pos) in range(1, 10) and self.board[int(pos)-1] == 0:
+                    break
+        return int(pos)
+
+    @staticmethod
+    def ask_for_repetition(string=None):
+        """repetition of the game after it's end"""
+        ans = ""
+        if string is None:
+            print("Уважаеммые игроки, хотите ли вы сыграть ещё одну партию?")
+            print("Если да, введите YES, иначе NO")
+            i = 0
+            inps = ["Если и сейчас не сможете ввести, крестики-нолики слишком сложая игра для вас:",
+                    "Ну всё, последняя попытка, я не шучу:",
+                    "Вы думаете, мы тут шутки шутим? Теперь точно последняя попытка:",
+                    "Ну ладно, даю вам шанс исправиться:"]
+            ans = input("Ваш ответ:")
+            while ans not in ("YES", "NO"):
+                if i == 0:
+                    print("У вас что, какие-то сложности с напиманием слова 'YES' или 'NO'?")
+                elif i == 4:
+                    print("Хорошо, всё с вами понятно...")
+                    ans = False
+                    break
+                ans = input(inps[i])
+                i += 1
+        else:
+            for ans in string:
+                if ans in ("YES", "NO"):
+                    break
+        if ans == "YES":
+            return True
+        return False
+
+    def win_move(self, pos):
+        """check the move"""
+        self.board[pos-1] = self.turn % 2 + 1
+        wline = list(filter(lambda x: (x-1) // 3 == (pos-1) // 3, range(pos-2, pos+3)))
+        wline = [self.board[i-1] for i in wline]
+        wline = reduce(lambda x, y: x*y, wline)
+        hline = list(set(range(pos-6, pos+7, 3)).intersection(range(1, 10)))
+        hline = [self.board[i-1] for i in hline]
+        hline = reduce(lambda x, y: x*y, hline)
+        ascline = [1, 5, 9]
+        if pos in ascline:
+            ascline = [self.board[i-1] for i in ascline]
+            ascline = reduce(lambda x, y: x*y, ascline)
+        else:
+            ascline = 0
+        descline = [3, 5, 7]
+        if pos in descline:
+            descline = [self.board[i-1] for i in descline]
+            descline = reduce(lambda x, y: x*y, descline)
+        else:
+            descline = 0
+        target = (self.turn % 2 + 1)**3
+        if target in [hline, wline, ascline, descline]:
+            return True
+        self.turn += 1
+        return False
+
+
+if __name__ == "__main__":
+    t = TicTacToe()
+    t.mainloop()
+    print("Спасибо, что поиграли в эту игру! До встречи!")
