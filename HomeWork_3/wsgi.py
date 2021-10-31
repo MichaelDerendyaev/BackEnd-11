f1b8283a39a5eb5febd249ba49853e2f58574ff7
MichaelDerendyaev <derendyaevm1@mail.ru>
2021-10-31 20:50:10 +0300

    HW3

--- HomeWork_3/wsgi.py
+++ HomeWork_3/wsgi.py
@@ -1,8 +1,27 @@
-def app(environ, start_response):
-    with open("public/Static.json", "rb") as f:
+import datetime
+import json
+
+
+def app_dynamic(env, start_response):
+    #print(env)
+    data = json.dumps({
+        'time': str(datetime.datetime.now()),
+        'url': "http://" + env["HTTP_HOST"] + env["RAW_URI"]
+    })
+    data = bytes(data, 'utf-8')
+    start_response("200 OK", [
+        ("Content-Type", "text/json"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
+
+
+def app_static(env, start_response):
+    #print(env)
+    with open("public/index.html", "rb") as f:
         data = f.read()
     start_response("200 OK", [
-        ("Content-Type", "json"),
+        ("Content-Type", "text/html"),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
